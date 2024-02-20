24ae653298bb69b6d8ff99b1da7ce7a8a5d8d534
Robert Spencer <spencer.robert.d@gmail.com>
2024-02-19 21:25:57 -0700

    basic floor skeleton

--- DungeonObj.py
+++ DungeonObj.py
@@ -1,5 +1,15 @@
+# encompasses the whole dungeon; floors, monsters, traps, hazards and loot
+from floor import Floor
 class Dungeon:
     def __init__(self, name, depth, level):
         self.name = name
         self.depth = depth
         self.level = level
+        self.floors = []
+        for i in range(self.depth):
+            self.new_floor()  # generates a new floor for the dungeon
+
+    def new_floor(self):  # TODO flesh out floor creation
+        self.floors.append(Floor())
+
+
---
+++ floor.py
@@ -0,0 +1,29 @@
+# a single floor in the dungeon. Has a description, threats, loot, passages, rooms, and hidden elements
+class Floor:
+    def __init__(self, description="", rooms=0, level=0, encounter_table=None, threat_table=None, passage_array=None,
+                 floor_traversal=None):
+        if floor_traversal is None:
+            floor_traversal = []
+        if passage_array is None:
+            passage_array = []
+        if threat_table is None:
+            threat_table = []
+        if encounter_table is None:
+            encounter_table = []
+
+        self.description = description  # the description of the floor. Displayed in the string
+        self.rooms = rooms  # the number of rooms in the dungeon floor
+        self.level = level  # the level of threat on the floor.
+        self.encounter_table = encounter_table  # a list of encounters for each room, the enemies in each room.
+        self.threat_table = threat_table  # the threats in the room. Both static hazards like lava, acid, pits. Also
+        # the traps leading into the room, e.g. tripwires, pressure plates. Only the traps leading out of the room,
+        # not in
+        self.passage_array = passage_array  # the rooms are a graph, and the passage array is the edges
+        # between vertices (rooms).
+        self.floor_traversal = floor_traversal  # an array of stair objects that take the party between floors.
+        # Todos: (all these classes must be made before floor can begin):
+        # TODO stairs obj.
+        # TODO encounter obj.
+        # TODO door trap obj.
+        # TODO hazard obj.
+        
